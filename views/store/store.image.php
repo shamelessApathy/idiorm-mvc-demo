8012ca0f80ca6c3fb153791c0904c43a54fd7d05
B <bcm811@gmail.com>
2017-01-27 15:37:22 -0700

    added payment processing functionality with stripe

--- views/store/store.image.php
+++ views/store/store.image.php
@@ -19,9 +19,10 @@ if (isset($_SESSION['user_info']))
 			<ul>
 			<?php echo "
 				<li>Uploaded By: </li>
-				<li >$user->username <img class='user_avatar2' src='$user->avatar'/></li>
+				<li>$user->username <img class='user_avatar2' src='$user->avatar'/></li>
 				<li>Name: $image->user_image_name</li>
 				<li>Dimensions: $image->width x $image->height</li>
+				<li id='price'>Price: $image->price</li>
 				
 			";
 			?>
@@ -29,7 +30,7 @@ if (isset($_SESSION['user_info']))
 			</div>
 			</div>
 		<div class='col-md-8'>
-			<img class='watermark' src="<?php echo $image->watermark;?>"/>
+			<img class='watermark' data-id="<?php echo $image->id;?>" src="<?php echo $image->watermark;?>"/>
 		</div>
 				<div class='col-md-2'>
 
@@ -55,10 +56,47 @@ if (isset($_SESSION['user_info']))
 			</div>
 	</div>
 </div>
-<form action='/image/buy' method='POST'>
+<form id='form_buy' action='/image/buy' method='POST'>
 <input name='image_id' hidden value="<?php echo $image->id;?>">
+<input id='token' name='token' hidden value="">
+<input id='price' name='price' hidden value="<?php echo $image->price;?>">
 <button type='submit'>BUY NOW</button>
 </form>
+<script src="https://checkout.stripe.com/checkout.js"></script>
+
+<button id="customButton">Purchase</button>
+
+<script>
+var handler = StripeCheckout.configure({
+  key: 'pk_test_THNgelG59hWBD2E9l16SZqsj',
+  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+  locale: 'auto',
+  token: function(token) {
+    // You can access the token ID with `token.id`.
+    // Get the token ID to your server-side code for use.
+    var token = token.id;
+    $('#token').val(token);
+    document.getElementById('form_buy').submit();
+    }
+  });
+
+document.getElementById('customButton').addEventListener('click', function(e) {
+  // Open Checkout with further options:
+  handler.open({
+    name: 'Stripe.com',
+    description: '2 widgets',
+    zipCode: true,
+    amount: <?php echo $image->price*100 ;?>,
+    description: "image_id =<?php echo $image->id ;?>"
+  });
+  e.preventDefault();
+});
+
+// Close Checkout on page navigation:
+window.addEventListener('popstate', function() {
+  handler.close();
+});
+</script>
 <form action='/image/subscription' method='POST'>
 	<input name='image_id' hidden value="<?php echo $image->id;?>">
 	<button type='submit'>USE SUBSCRIPTION</button>
