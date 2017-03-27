fce888a078d6b84d3907509547f6430f58919732
B <bcm811@gmail.com>
2017-03-27 12:03:56 -0600

    re-added index.php, also composer installed phpdotenv to use for creds

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+//require('vendor/j4mie/idiorm/idiorm.php');
+//require('vendor/j4mie/paris/paris.php');
+require('vendor/autoload.php');
+require('constants.php');
+require('globals.php');
+require('uri_router.php');
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+ORM::configure('mysql:host=localhost;dbname=idiorm');
+ORM::configure('username', $_ENV['DB_USER']);
+ORM::configure('password', $_ENV['DB_PASS']);
+ORM::configure('logging', true);
+ORM::configure('error_mode', PDO::ERRMODE_SILENT);
+ORM::configure('id_column_overrides', array(
+    'album' => 'album_id',
+    'user' => 'user_id'
+));
+session_start();
+$_SESSION['start'] = true;
+
+$uri = $_SERVER['REQUEST_URI'];
+if (strpos($uri, '?'))
+{
+	$explode = explode('?',$uri);
+	$uri = $explode[0];
+	$query = $explode[1];
+	uri_router($uri, $query);
+}
+else
+{
+uri_router($uri);
+}
+
+
+
+
+?>
