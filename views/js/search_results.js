5586227190d9de8a406aa38150bb3b05258e2a23
B <bcm811@gmail.com>
2017-05-11 12:31:26 -0600

    got a decent item details UI working for the image_search_Results page, just needs to be mobile now

--- views/js/search_results.js
+++ views/js/search_results.js
@@ -8,7 +8,13 @@ $(function(){
 			this.add_to_cart = $('.add_to_cart');
 			this.tags = $(this.element).attr('data-tags');
 			this.json = $.parseJSON(this.tags);
-			this.tag_holder = $('.item-details-tags');
+			this.tag_holder = $('.image-details-tags');
+			this.tag_string = "<h4>TAGS</h4>";
+			for (var i = 0; i < this.json.length; i++)
+			{
+				var wrapper = "<div class='tag'>" + this.json[i]['text'] + "</div>";
+				this.tag_string = this.tag_string + wrapper;
+			}
 			this.width = $(this.element).attr('data-width');
 			this.image_id = $(this.element).attr('data-id');
 			this.height = $(this.element).attr('data-height');
@@ -18,12 +24,13 @@ $(function(){
 			this.price_holder = $('.image-details-price');
 			this.url = $(this.element).attr('src');
 			this.image_holder = $('.image-details-holder');
+			this.button = $('.add_to_cart');
 			this.hider = $('.image-details-hider');
 		}
 		this.open = function()
 		{
 			console.log('running');
-			$(this.area).css({"height":"300px"});
+			$(this.area).css({"height":"350px"});
 			$(this.hider).css({"display":"block"});
 			this.addDetails();
 		}.bind(this);
@@ -39,7 +46,7 @@ $(function(){
 		}.bind(this)
 		this.addDetails = function()
 		{
-			$(this.image_holder).html("<img style='max-width:100%;' src='"+this.url+"'/>");
+			$(this.image_holder).html("<a href='/image/info?id="+this.image_id+"'><img style='max-width:100%;' src='"+this.url+"'/></a>");
 			var price = "Price " + this.price + ".00";
 			var filetype = "Filetype " + this.filetype;
 			var width = "Width " + this.width + "px";
@@ -49,9 +56,11 @@ $(function(){
 			$(this.filetype_holder).html(filetype);
 			$(this.width_holder).html(width);
 			$(this.height_holder).html(height);
-			console.log(this.tags);
+			$(this.button).attr('data-id', this.image_id);
+			console.log(this.tag_string);
+			$(this.tag_holder).html(this.tag_string);
 //			$('.item-details-price').html(this.price);
-		}
+		}.bind(this)
 		this.initialize();
 	}
 var elements = document.getElementsByClassName('image-result');
--- views/view.image_search_results.php
+++ views/view.image_search_results.php
@@ -14,10 +14,11 @@ if (!empty($info['images']))
 ?>
 <div class='image-details'>
 <div class='image-details-hider'>
+<div class='image-button-holder'><button type='button' class='add_to_cart'>Add To Cart</button></div>
 <div class='close-image-details'>X</div>
+<div class='clear'></div>
 <div class='row'>
 <div class='col-md-3'>
-<div class='clear'></div>
 
 <ul class='image-details-ul'>
 		<li class='image-details-price'></li>
@@ -31,7 +32,7 @@ if (!empty($info['images']))
 
 
 	<div class='image-details-holder'></div>
-	<button style='display:inline; position:relative;' class="add_to_cart" >Add to Cart</button>
+	<div class='image-details-cart'></div>
 </div>
 </div>
 </div>
@@ -49,7 +50,7 @@ if (!empty($info['images']))
 				data-id="<?php echo $image->info->id;?>"
 				data-tags="<?php $json = json_encode($image['tags']);echo htmlentities($json, ENT_QUOTES, 'UTF-8');?>"
 				src="<?php echo $image->thumbnail;?>"/>
-		</div>
+		</div> 
 
 </div>
 <?php endforeach;?>
