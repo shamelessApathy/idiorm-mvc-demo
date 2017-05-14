5429acab8c22cc2473ee6fe16b671079076ae8ee
shamelessApathy <shamelessApathy>
2017-05-14 23:18:09 +0000

    changed around some OG markup stuff

--- views/header.php
+++ views/header.php
@@ -5,20 +5,28 @@ $cart2 = new Cart();
  ?>
 <html>
 <head>
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1">-->
-<link href="<?php echo '/views/bootstrap/css/bootstrap.css';?>" rel='stylesheet' type='text/css'/>
-<link href='/views/js/jquery-ui-1.12.1/jquery-ui.css' rel='stylesheet' type='text/css'/>
-<link href="<?php echo '/'. CSS . '/styles.css';?>" rel='stylesheet' type='text/css'/>
-<link href='/views/css/font-awesome-4.7.0/css/font-awesome.min.css' type='text/css' rel='stylesheet'/>
+
 <?php if (is_array($info) && isset($info['image'])) : ?>
   <?php $image = $info['image'];
 $image_url = "http://dev.sharefuly.com" . $image->watermark;
 $path = "/image/info?id=";
+$request =$_SERVER['REQUEST_URI'];
+$exploded = explode('/',$request);
+$second_explode = explode('?',$exploded[2]);
+$which = $second_explode[0];
+$page_url = "http://dev.sharefuly.com/image/info?id=".$image->id;
+$description = "";
+foreach ($info['tags'] as $tag)
+{
+  $description = $description.$tag['text'].", ";
+}
+
    ?>
 <meta property="og:title" content="<?php echo $image->user_image_name;?>" />
 <meta property="og:type" content="website"/>  
 <meta property="og:image"              content="<?php echo $image_url;?>"/>
-<meta property="og:description"        content="This is the description for a stock photography excerpt"/>
+<meta property="og:url"              content="<?php echo $page_url;?>"/>
+<meta property="og:description"        content="<?php echo $description;?>"/>
 <meta property="og:site_name"              content="Sharefuly"/>
 <?php else: ?>
   <?php $server = $_SERVER['SERVER_NAME']; 
@@ -30,6 +38,11 @@ $path = "/image/info?id=";
 <meta property="og:description"        content="Stock photo website anyone can contribute to!"/>
 <meta property="og:site_name"              content="Sharefuly"/>
 <?php endif; ?>
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<link href="<?php echo '/views/bootstrap/css/bootstrap.css';?>" rel='stylesheet' type='text/css'/>
+<link href='/views/js/jquery-ui-1.12.1/jquery-ui.css' rel='stylesheet' type='text/css'/>
+<link href="<?php echo '/'. CSS . '/styles.css';?>" rel='stylesheet' type='text/css'/>
+<link href='/views/css/font-awesome-4.7.0/css/font-awesome.min.css' type='text/css' rel='stylesheet'/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 </head>
