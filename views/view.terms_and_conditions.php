0a19ab2ac6cf7f5d25035e53c47e815915e4e5e6
B <bcm811@gmail.com>
2017-06-01 11:17:54 -0600

    added the Terms of Use section to the 'terms and conditions' area, need to sitll add the other two aggreements, needs formatting

--- views/view.terms_and_conditions.php
+++ views/view.terms_and_conditions.php
@@ -7,8 +7,80 @@
 		</div>
 		<div class='user-agreement-inside'>
 		<div class='user-agreement-slider-hider'></div>
-			<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-			<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+	<h3 style='text-align:center;'>Terms of Use</h3> 
+	<p style='text-align:center;'><sub>Last Revised: April 14, 2017</sub></p>
+
+	<h5>Purpose of Sharefuly</h5>
+	<p>
+	Sharefuly is an image hosting website that is here to host media, advertising, and related content. We help everyday people like you connect with others to share, buy, and sell original digital media. The media and content are created and developed by and for users.</p>
+
+	<h5>Purpose of Terms of Use</h5>
+	<p>The Terms of Use (“Terms”) are the rules that you must follow and agree to abide by in order to use the services provided by Sharefuly and the site (“Site”). The Terms are a legally binding contract between you and Sharefuly made up of this document and others referenced below. This contract sets out your rights and responsibilities when you use the services provided by Sharefuly (“Services”), including accessing our servers, viewing our websites or content, browsing pictures and media, buying products, and selling products to other users. By using our Site or Services or accessing our servers or content in any way, you are agreeing to comply with these Terms. These Terms apply to anyone who visits this Site, anyone who views this Site, all users, all Buyers, and all Sellers. Please read them carefully.</p>
+
+	<p>These terms may change from time to time, and we will post a revised version. We don’t mean to yell at you when we write things in BOLD; these sections are just important (and our legal team made us do it). YOUR CONTINUED USE OF ANY OF THE SITES AND APPS AFTER WE POST ANY CHANGES WILL CONSTITUTE YOUR ACCEPTANCE OF SUCH CHANGES. IN ADDITION, BY ORDERING PRODUCTS OR USING SERVICES, YOU ACKNOWLEDGE THAT YOU HAVE READ AND REVIEWED THESE TERMS IN THEIR ENTIRETY, YOU AGREE TO THESE TERMS AND THE PRIVACY POLICY AND THESE TERMS CONSTITUTE BINDING AND ENFORCEABLE OBLIGATIONS ON YOU. IF YOU DO NOT AGREE TO ALL OF THE TERMS AND CONDITIONS CONTAINED IN THE TERMS OF USE, DO NTO ACCESS OR USE THIS SITE.</p>
+
+	<h5>Using the Site and Your Account</h5>
+	<p>You must be 18 or older to use our Services. If you are under 18, you may only use our Services under the supervision of a parent or legal guardian. If you are registering as a business entity, you personally guarantee that you have the authority to agree to the Terms on behalf of the business. You are solely responsible for any activity on your account, and you cannot transfer your account to anyone. Protect your password. You also agree not to collect users' personal and/or contact information. These Terms do not create any agency, partnership, joint venture, employment or franchisee relationship.</p>
+
+	<p>You agree not to use or provide software (except for general purpose web browsers and email clients, or software expressly licensed by us) or services that interact or interoperate with Sharefuly, such as for downloading, uploading, posting, flagging, emailing, search, or mobile use. Robots, spiders, scripts, scrapers, crawlers, etc. are not allowed, as are misleading, unsolicited, unlawful, and/or spam postings/email. </p>
+
+	<p>Sharefuly can cancel your account for any violation of these Terms or any other reason, including, but not limited to, manipulating relative rankings based on tag votes or acting maliciously.</p>
+
+	<h5>Your Content and Conduct</h5>
+	<p>Content that you post using our Services is your content. You represent that you have all rights to your content and that you are not infringing or violating any other user’s or third party’s rights by posting it, including intellectual property rights and privacy rights. You are the sole owner and solely responsible for it. By posting content, you grant us a non-exclusive, worldwide, royalty-free, irrevocable, sub-licensable, perpetual license to use, display, edit, modify, reproduce, distribute, or store it. We do not exert ownership over it and will not sell it ourselves. You agree that Sharefuly has sole discretion to filter, delete, verify, or delay your content. You agree that you will not do any of the following while using the Site:
+	1. Post, upload, advertise, or solicit content that exploits minors or is illegal, inappropriate, vulgar, sexually explicit, lewd, obscene, pornographic, is deemed to be in bad taste, or does not align with Sharefuly’s values;
+	2. Use, post, upload, advertise, or solicit someone else's image, name, likeness, or other personal attributes without permission or consent;
+	3. Harass, threaten, embarrass, or cause distress or discomfort to another individual or entity or impersonate any other person or entity or restrict or deter any other person from using or enjoying the Site;
+	4. Transmit any information, data, text, files, links, software, chats, communication or other materials that are unlawful, false, misleading, harmful, threatening, abusive, invasive of another's privacy, harassing, defamatory, vulgar, obscene, hateful or racially or otherwise objectionable, including without limitation material of any kind or nature that encourages conduct that could constitute a criminal offense, give rise to civil liability or otherwise violate any applicable local, state, provincial, national, or international law or regulation;
+	5. Infringe or interfere with the patents, copyrights, trademarks, service marks, logos, confidential information or intellectual property rights of others;
+	6. Use any automated device, spider, robot, crawler, data mining tool, software or routine to access, copy, or download any part of the Site;
+	7. Take any action creating a disproportionately large usage load on the Site;
+	8. Send messages or engaging in disruptive or damaging activities online, including excessive use of scripts, sound waves, scrolling, or use of viruses, bots, worms, time bombs, Trojan horses or any other destructive element;
+	9. Interfere with the Site in any way such as decipher, decompile, disassemble or reverse engineer any of the software comprising or in any way making up all or any part of the Site;
+	10. Post links to commercial services or web sites;
+	11. Engage in or assist another individual or entity to engage in fraudulent, abusive, manipulative or illegal activity.
+
+	Please report any violations of these Terms to CONTACT INFO.
+</p>
+	<h5>Selling Your Content</h5>
+	<p>
+	What you are selling is a license to use your content, which is explained in greater detail below. The buyer (“Buyer”) does not have the right to then post your content on the Site and present it as his or her own. </p>
+
+	<h5>Purchasing Content</h5>
+	<p>The Buyer who purchases content on Sharefuly is purchasing a license to use the content, explained below. The purchase price is a licensing fee, which grants the Buyer the right to use the content but does not grant title to the Buyer. Buyers may not sell or assign this license to third parties. Buyers can access all of their purchased images on the website, but access to these images is not guaranteed.</p>
+
+	<h5>License</h5>
+	<p>This is a license to use our Site. Sharefuly gives you a limited, revocable, nonexclusive, nonassignable, nonsublicensable license to access the Site on the condition that you abide by these Terms. You and other users do not have unlicensed access. Sharefuly remains the sole owner of all right, title, and interest in the Site and reserves all rights not expressly granted under these Terms of Use. You cannot license, distribute, make derivative works, display, sell, or frame content from Sharefuly, with the exception of content that you create and share with friends or family.</p>
+
+	<p>By posting content, you grant Sharefuly a non-exclusive, worldwide, royalty-free, irrevocable, sub-licensable, perpetual license to use, display, edit, modify, reproduce, distribute, or store it. You allow us to use your images for the purpose of advertising and promotion of this Site. Users also agree to grant Buyers a perpetual, irrevocable license upon payment of the agreed upon purchase price, or licensing fee. This license grants Buyers the ability to use the content for any purpose that does not violate these Terms, your rights, or the rights of others. Buyers are not granted title to the content and cannot sell your work as his or her own or assign the license to a third party. The license will continue unless and until you remove your content from the Site. You may not remove any watermarks or copyright notices contained in content on this Site. Notwithstanding the foregoing, the license for legal archival/preservation purposes will continue indefinitely.</p>
+
+	<h5>Payment Terms and What We Charge to Sell Your Content</h5>
+	<p>Sharefuly uses Square, Inc. or PayPal® and only uses U.S. Dollars. To make purchases or collect licensing fees, you must have a valid Square or PayPal® account on file. Payments will be made on a NET30+7 time schedule. This means that Sharefuly has 30 days from the end of the payment month after an item was purchased plus 7 days to deposit funds into your account. We will try to pay you as quickly as possible, but this ensures that you get all of the payments that you have earned. Final payment amounts may differ from the purchase tracking user page.</p>
+
+	<p>Sharefuly charges a listing fee, which will only be charged when you sell an item. You authorize us to collect a listing fee as a percentage of each sale that you make. The listing fee is 10% of the listed purchase price for each item. You can list an item for any amount and for whatever you believe that it is worth. For example, if you sell an item that you listed for $10.00, you will collect $9.00, and Sharefuly will collect $1.00. If you sell an item that you listed for $1,000.00, you will collect $900.00, and Sharefuly will collect $100.00.</p>
+
+	<p>Buyers will be charged a $0.30 fee for any purchases under $10.00. </p>
+
+	<h5>Subscriptions</h5>
+	<p>Buyers and Sellers may elect to join one of Sharefuly’s subscription services. </p>
+
+	<h5>Copyright</h5>
+	<p>Copyright is a legal right created by law that grants the creator of an original work exclusive rights for its use and distribution. Sharefuly is committed to protecting your intellectual property rights.  You assume all responsibility for the content that you upload. In the event that a copyright infringement claim is filed against you, your image(s) will be disabled until you authenticate and prove that the image is yours. If your submission is proven to have infringed on the rights of another, your user information will be given to the affected party. </p>
+
+	<p>Please report any instances of copyright infringement of your or another’s work to CONTACT INFO. We will report and quickly respond to your concerns. Feel free to visit https://www.copyright.gov/ for additional information about Copyright Law of the United States and how you can protect your work. </p>
+
+	<h5>Disputes and Applicable Law</h5>
+	<p>All disputes between you and Sharefuly, controversy or claim arising out of or relating to this Agreement, or the breach thereof, shall be settled by binding arbitration administered under the Commercial Arbitration Rules of the American Arbitration Association or of the International Centre for Dispute Resolution in effect on the date of the commencement of arbitration, rather than in court, and judgment on the award rendered by the arbitrator(s) may be entered in any court having jurisdiction thereof or having jurisdiction over the relevant party or its assets. The place of arbitration shall be the state of Utah. Each party will bear its own costs in the arbitration. Sharefuly also has the right to commence and prosecute any legal or equitable action or proceeding before any court of competent jurisdiction to obtain injunctive or other relief against you in the event that such action is necessary or desirable.</p>
+
+	<h5>Indemnification</h5>
+	<p>
+	You agree to defend, indemnify and hold harmless Sharefuy, its subsidiaries, affiliates, licensors, employees, agents, third party information providers, and independent contractors against any claims, damages, costs, liabilities and expenses (including, but not limited to, reasonable attorneys' fees) arising out of or related to your conduct, your use or inability to use Site, your breach or alleged breach of the Terms of Use or of any representation or warranty contained herein, your unauthorized use of the Sharefuly content, or your violation of any rights of another.</p>
+
+
+
+
+
+
 		</div>
 	</div>
 
