ec228477567469f8502ad4736a31e2b145957e90
shamelessApathy <bcm811@gmail.com>
2018-02-10 12:43:01 -0700

    changed the way search results are displayed, it links directly to image rather than have any fancy aNIMATION

--- views/view.category.php
+++ views/view.category.php
@@ -6,41 +6,14 @@ print_r($images);
 echo "</pre>";*/
 
 ?>
-<div class='image-details'>
-<div class='image-details-hider'>
-<div class='image-details-opacity'>
-<div class='image-button-holder'><a href='/login'><button type='button'>Login to Download</button></a></div>
-<div class='close-image-details'><i class='fa fa-close'></i></div>
-<div class='clear'></div>
-<div class='row'>
-<div class='col-md-3'>
-
-<ul class='image-details-ul'>
-		<li class='image-details-username'></li>
-		<li class='image-details-filtype'></li>
-		<li class='image-details-width'></li>
-		<li class='image-details-height'></li>
-		<li class='image-details-tags'></li>
-</ul>
-</div>
-<div class='col-md-9'>
-
-
-	<div class='image-details-holder'></div>
-	<div class='image-details-cart'></div>
-</div>
-</div>
-</div>
-</div>
-</div>
 <div class='container'>
 	<div class='row'>
 	<h1><?php echo strtoupper($info['category']);?></h1>
 		<?php foreach($images as $image):?>
 			<?php if (!empty($image)):?>
 			<div class='search-item'>
-			<div class='preview-thumb'> 
-				<a href="/image/info?id=<?php echo $image->id;?>" class='search-item-link'></a>
+				<a href="/image/info?id=<?php echo $image->id;?>" class='search-item-link'>
+			<div class='preview-thumb'> 				
 			<img class='image-result' 
 				alt="<?php echo $image->user_image_name;?>"
 				data-user="<?php echo $image->user_id;?>"
@@ -54,7 +27,7 @@ echo "</pre>";*/
 				data-tags="<?php $json = json_encode($image->tags);echo htmlentities($json, ENT_QUOTES, 'UTF-8');?>"
 				data-watermark="<?php echo $image->watermark;?>"
 				src="<?php echo $image->thumbnail;?>"/>
-			</div>
+			</div></a>
 			</div>
 		<?php endif;?>
 		<?php endforeach;?>
@@ -62,4 +35,3 @@ echo "</pre>";*/
 </div>
 <?php require_once(HTML_FOOTER);?>
 <?php require_once(FOOTER);?> 
-<script src='/views/js/search_results.js'></script>
--- views/view.image_search_results.php
+++ views/view.image_search_results.php
@@ -1,36 +1,6 @@
 <?php require(HEADER);
 ?>
-<div class='image-details'>
-<div class='image-details-hider'>
-<div class='image-details-opacity'>
-<?php if(isset($_SESSION['user_info']['id'])):?>
-<div class='image-button-holder'><a id='image-download-link'><button type='button' class='download-button'>Download</button></div></a>
-<?php else:?>
-<div class='image-button-holder'><a href='/login'><button type='button' class='login-button'>Login to Download</button></div></a>
-<?php endif;?>
-<div class='close-image-details'><i class='fa fa-close'></i></div>
-<div class='clear'></div>
-<div class='row'>
-<div class='col-md-3'>
 
-<ul class='image-details-ul'>
-		<li class='image-details-price'></li>
-		<li class='image-details-filtype'></li>
-		<li class='image-details-width'></li>
-		<li class='image-details-height'></li>
-		<li class='image-details-tags'></li>
-</ul>
-</div>
-<div class='col-md-9'>
-
-
-	<div class='image-details-holder'></div>
-	<div class='image-details-cart'></div>
-</div>
-</div>
-</div>
-</div>
-</div>
 <div class='container'>
 
 <h3>Search Results for "<?php echo $info['query'];?>"</h3>
@@ -48,8 +18,9 @@ if (!empty($info['images']))
 <?php if (is_array($info) && !empty($info['images'])):?>
 <?php foreach($images as $image):?>
 <div class='search-item'>
+	<a href="/image/info?id=<?php echo $image->id;?>" class='search-item-link'>
 		<div class='preview-thumb'>
-			<a href="/image/info?id=<?php echo $image->info->id;?>" class='search-item-link'></a>
+			<a href="/image/info?id=<?php echo $image->info->id;?>" class='search-item-link'>
 			<img class='image-result' 
 				data-price="<?php echo $image->info->price;?>" 
 				data-filetype="<?php echo $image->info->mime_type;?>" 
@@ -59,7 +30,7 @@ if (!empty($info['images']))
 				data-tags="<?php $json = json_encode($image['tags']);echo htmlentities($json, ENT_QUOTES, 'UTF-8');?>"
 				data-watermark="<?php echo $image->info->watermark;?>"
 				src="<?php echo $image->thumbnail;?>"/>
-		</div> 
+		</div></a>
 
 </div>
 <?php endforeach;?>
@@ -84,4 +55,3 @@ for ($i = 1; $i <= $num_pages; $i++)
  </div>
  <?php require_once(HTML_FOOTER); ?>
 <?php require(FOOTER); ?>
-<script src='/views/js/search_results.js'></script>
