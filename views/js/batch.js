23e7a5b12a41394c5480faf935b6c8ea0cc30195
shamelessApathy <bcm811@gmail.com>
2018-02-23 13:27:17 -0700

    2nd incarnaation of batch uploads

--- views/js/batch.js
+++ views/js/batch.js
@@ -9,33 +9,8 @@ $(function(){
 		}
 		this.listen = function()
 		{
-			$(this.form).on('change', function(e){
-				console.log('it changed');
-				var files = $('#batch-file').prop("files")
-				var names = $.map(files, function(val) { return val.name; });
-				this.previewBatch(files);
-
-			}.bind(this));
+			console.log('listeners active');
 		}
-		this.dataURL = function(file)
-		{
-			var reader = new FileReader();
-			var src;
-			reader.onload = function(file)
-			{
-				var source = file.currentTarget.result;
-				$('.batch-upload-card-holder').append('<div class="batch-upload-card"><br><div class="batch-img-holder"><img style="max-width:100%;" src="'+source+'" /></div><div class="batch-image-info"></div></div>');
-			}
-			var dataURL = reader.readAsDataURL(file);
-		}
-		this.previewBatch = function(list)
-		{
-			var cards = document.getElementsByClassName('batch-upload-card');
-			for (var i=0; i < list.length; i++)
-			{
-				this.dataURL(list[i]);
-			}
-		}.bind(this)
 		this.init();
 		this.listen();
 	}
--- views/view.test.php
+++ views/view.test.php
@@ -3,19 +3,19 @@ require(HEADER);
 ?>
 <h1>You are in the batch upload test area</h1>
 
-<form enctype='multipart/form-data' name='test-batch' id='test-batch' method="POST" action='/test/catch_batch'>
-	<input type='file' name='batch-file' id='batch-file' multiple/>
-	<div class='batch-upload-card-holder'></div>
-	<button type='button'>Submit</button>
+<form enctype="multipart/form-data" name="test-batch" id="test-batch" method="POST" target="batch-upload-iframe" action="/test/catch_batch">
+	<input type="file" name="batch-file[]" id="batch-file" multiple/>
+	<div class="batch-upload-card-holder"></div>
+	<button type="submit">Submit</button>
 </form>
 
 
 <div id='files'>
-
+ 
 </div>
 
 
-
+<iframe name="batch-upload-iframe" id="batch-upload-iframe"></iframe>
 
 
 <?php 
