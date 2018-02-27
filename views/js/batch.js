936f8a16ca89818b4462130256be4eba09d1bfb9
shamelessApathy <bcm811@gmail.com>
2018-02-27 12:45:33 -0700

    got the cards starting to form forms

--- views/js/batch.js
+++ views/js/batch.js
@@ -19,10 +19,21 @@ $(function(){
 			var return_data = $(doc).children().children()[1];
 			var body = return_data.innerHTML;
 			var return_final = JSON.parse(body);
+			var card_holder = document.getElementById('files');
 			console.log(return_final);
+			for ( var i = 0; i < return_final.length; i++)
+			{
+				var id = return_final[i]['id'];
+				var string = "<div class='batch-upload-card' data-id='"+return_final[i]['id']+"''><div class='batch-img-holder'><img style='max-width:100%; max-height:100%;' src='"+return_final[i]['path']+"'/></div><input type='number' name='images["+id+"]' value='"+id+"' HIDDEN/><label>Image Name</label><br><input type='text' name='images["+id+"][name]'/></div>";
+				card_holder.innerHTML += string; 
+			}
+			var final_button = document.getElementById('final-cut-button');
+			var first_button = document.getElementById('first-submit');
+			first_button.classList.add('invisible');
+			final_button.classList.remove('invisible');
 		}
 		this.init();
 		this.listen();
-	}
+	} 
 	var upload = new BatchUpload();
-}) 
+});
--- views/view.test.php
+++ views/view.test.php
@@ -6,13 +6,15 @@ require(HEADER);
 <form enctype="multipart/form-data" name="test-batch" id="test-batch" method="POST" target="batch-upload-iframe" action="/test/catch_batch">
 	<input type="file" name="batch-file[]" id="batch-file" multiple/>
 	<div class="batch-upload-card-holder"></div>
-	<button type="submit">Submit</button>
+	<button id='first-submit' type="submit">Submit</button>
 </form>
 
-
+<form id='final-cut' name='images[]' action='/test/final_cut' method='POST'>
 <div id='files'>
  
 </div>
+<button class='invisible' type='submit' id='final-cut-button'>Submit</button>
+</form>
 
 
 <iframe name="batch-upload-iframe" id="batch-upload-iframe"></iframe>
